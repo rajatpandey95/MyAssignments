fed53c1d80e16f70cff6e8be21c230b8a05a4a4d
rajatpandey95 <rajat.pandey_cs19@gla.ac.in>
2023-04-28 22:21:29 +0530

    question4

---
+++ assignment_1/question4.java
@@ -0,0 +1,30 @@
+package assignment_1;
+
+import java.util.Scanner;
+
+class ResultDeclaration {
+    public String declareResults(double subject1Marks, double subject2Marks, double subject3Marks) {
+        if (subject1Marks > 60 && subject2Marks > 60 && subject3Marks > 60) {
+            return "Promoted";
+        }
+        if ((subject1Marks > 60 && subject2Marks > 60) || (subject2Marks > 60 && subject3Marks > 60)
+                || (subject1Marks > 60 && subject3Marks > 60)) {
+            return "Passed";
+        }
+
+        return "failed";
+    }
+}
+
+public class question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sub1Marks = sc.nextInt(), sub2Marks = sc.nextInt(), sub3Marks = sc.nextInt();
+
+        ResultDeclaration obj = new ResultDeclaration();
+        System.out.println(obj.declareResults(sub1Marks, sub2Marks, sub3Marks));
+
+        sc.close();
+    }
+}
