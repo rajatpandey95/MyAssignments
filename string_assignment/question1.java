fe7673b47494a187e690aae8e975462db354f203
rajatpandey95 <rajat.pandey_cs19@gla.ac.in>
2023-05-15 00:23:18 +0530

    strings_assignment

---
+++ string_assignment/question1.java
@@ -0,0 +1,11 @@
+package string_assignment;
+
+public class question1 {
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        int len = str.length();
+        System.out.println(len);
+    }
+}
---
+++ string_assignment/question2.java
@@ -0,0 +1,14 @@
+package string_assignment;
+
+public class question2 {
+    public static void main(String[] args) {
+        String str1 = "Hello, ";
+        String str2 = "How are you?";
+
+        // .concat() method
+        String str3 = str1.concat(str2);
+
+        System.out.println(str3);
+
+    }
+}
