80ce404f4060d4d18634fb44583e0366e4f0cb43
rajatpandey95 <rajat.pandey_cs19@gla.ac.in>
2023-05-15 00:19:34 +0530

    StringBuffer

---
+++ stringBuffer_assignment/question1.java
@@ -0,0 +1,16 @@
+package stringBuffer_assignment;
+
+public class question1 {
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("StringBuffer");
+        sb.append("is a peer class of String");
+        sb.append("that provides much of");
+        sb.append("the functionality of strings");
+
+        System.out.println(sb.toString());
+
+    }
+}
