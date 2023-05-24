02589bd28ed7a7772ecd439cf87572433270791a
rajatpandey95 <rajat.pandey_cs19@gla.ac.in>
2023-05-24 14:41:39 +0530

    Generics

--- generics_assignment/question4.java
+++ generics_assignment/question4.java
@@ -2,15 +2,15 @@ package generics_assignment;
 
 import java.util.Date;
 
-class Pair<T> {
-    String key;
-    T value;
+class Pair<K, V> {
+    K key;
+    V value;
 
-    public void setKey(String key) {
+    public void setKey(K key) {
         this.key = key;
     }
 
-    public void setValue(T value) {
+    public void setValue(V value) {
         this.value = value;
     }
 }
@@ -19,11 +19,11 @@ public class question4 {
     public static void main(String[] args) {
 
         // Restriction is provided by passing date type as generics
-        Pair<String> myObj1 = new Pair<String>();
+        Pair<String, String> myObj1 = new Pair<String, String>();
         myObj1.setKey("1");
         myObj1.setValue("Hello");
 
-        Pair<Date> myObj2 = new Pair<Date>();
+        Pair<String, Date> myObj2 = new Pair<String, Date>();
         myObj2.setKey("Today is");
         myObj2.setValue(new Date());
 
